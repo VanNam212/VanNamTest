9820a3295a96cb98fbc9192a5918c136083dca30
VanNam212 <48545931+VanNam212@users.noreply.github.com>
2019-07-07 21:03:48 +0700

    Update SoNguyenTo.java

--- src/SoNguyenTo.java
+++ src/SoNguyenTo.java
@@ -3,29 +3,42 @@ import java.util.Scanner;
 public class SoNguyenTo {
     public static void main(String[] args) {
         int n;
-        int sum = 0;
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap so phan tu cua mang: ");
-        n = scanner.nextInt();
+        n = new Scanner(System.in).nextInt();
         int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            System.out.print("Nhap phan tu thu " + (i + 1) + " : ");
-            arr[i] = scanner.nextInt();
-        }
-        System.out.print("Nhap vua nhap la: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        nhap_mang(arr);
+        
+        System.out.print("\nMang vua nhap la: ");
+        xuat_mang(arr);
+        
         System.out.println();
         System.out.print("So nguyen to trong day la: ");
-        for (int i = 0; i < n; i++) {
+        xuat_SoNto(arr);
+    }
+    
+    public static void nhap_mang(int[] arr) {
+        for (int i = 0; i < arr.length(); i++) {
+            System.out.print("Nhap phan tu thu " + (i + 1) + " : ");
+            arr[i] = new Scanner(System.in).nextInt();
+        }    
+    }
+    
+    public static void xuat_mang(int[] arr) {
+        for (int i = 0; i < arr.length(); i++) {
+            System.out.print(arr[i] + " ");
+        }    
+    }
+    
+    public static void xuat_SoNto(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length(); i++) {
             if (SoNguyenTo(arr[i])) {
                 System.out.print(arr[i] + " ");
                 sum += arr[i];
             }
         }
         System.out.println();
-        System.out.println("Tong so cac so nguyen to trong day la: " + sum);
+        System.out.println("Tong so cac so nguyen to trong day la: " + sum);    
     }
 
     public static boolean SoNguyenTo(int n) {
